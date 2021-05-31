385727b6be969a323a83c9b871e4166d24bce948
archiblue <archiblue@naver.com>
2021-05-31 15:59:07 +0900

    post 생성 push

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요. nodemon install')
 })
 
-app.post('/register',(req,res) =>{
+app.post('/register',(req, res) =>{
   //회원가입할 때 필요한 정보들을 client에서 수신하면 DB에 Insert
+   console.log('app.Post :: Connection..1')
+
   const user = new User(req.body)
 
+  console.log(User.name);
+
   user.save((err,userInfo) => {
+     console.log('app.Post :: Connection..')
       if(err) return res.json({ success : false, err})
       return res.status(200).json({
         success: true
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +33,26 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+
+})
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    //password만 변경될때만 동작
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                
+                if(err) return next(err)
+                user.password = hash
+                next()
+                // Store hash in your password DB.
+            })
+        })
+    }
+    //비밀번호를 암호화 시킨다. 
+
 
 })
 
